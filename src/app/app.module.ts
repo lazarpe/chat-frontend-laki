5ac1015374536d102d32cca2dcc302fbbfdf561b
lazarpe <lazar.petrovic.nefti@gmail.com>
2021-12-07 15:48:49 +0100

    cool

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { ChatInboxComponent } from './chat-inbox/chat-inbox.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ChatInboxComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/chat-inbox/chat-inbox.component.ts
@@ -0,0 +1,47 @@
+import {Component, OnInit} from '@angular/core';
+import io from 'socket.io-client';
+
+const SOCKET_ENDPOINT = 'chat-app-laki.herokuapp.com';
+@Component({
+  selector: 'app-chat-inbox',
+  templateUrl: './chat-inbox.component.html',
+  styleUrls: ['./chat-inbox.component.css']
+})
+export class ChatInboxComponent implements OnInit {
+  socket: any;
+  message: string = "";
+  constructor() { }
+
+  ngOnInit() {
+    this.setupSocketConnection();
+  }
+
+  setupSocketConnection() {
+    // @ts-ignore
+    this.socket = io(SOCKET_ENDPOINT);
+    this.socket.on('message-broadcast', (data: string) => {
+      if (data) {
+        const element = document.createElement('li');
+        element.innerHTML = data;
+        element.style.background = 'white';
+        element.style.padding =  '15px 30px';
+        element.style.margin = '10px';
+        // @ts-ignore
+        document.getElementById('message-list').appendChild(element);
+      }
+    });
+  }
+
+  SendMessage() {
+    this.socket.emit('message', this.message);
+    const element = document.createElement('li');
+    element.innerHTML = this.message;
+    element.style.background = 'white';
+    element.style.padding =  '15px 30px';
+    element.style.margin = '10px';
+    element.style.textAlign = 'right';
+    // @ts-ignore
+    document.getElementById('message-list').appendChild(element);
+    this.message = '';
+  }
+}
